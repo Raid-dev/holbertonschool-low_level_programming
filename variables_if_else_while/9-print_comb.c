ba5515ecff91b1d505ccbe183478c0dd69e5f252
Raid-dev <a_rauf2004@mail.ru>
2024-02-23 17:32:02 +0000

    Added executable program script

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int i = 0;
 
-	putchar(i);
+	putchar(i + '0');
 	putchar(',');
 
 	i = 1;
@@ -18,7 +18,7 @@ int main(void)
 	while (i < 10)
 	{
 		putchar(' ');
-		putchar(i);
+		putchar(i + '0');
 		
 		i++;
 	}
