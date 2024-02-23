150b4886d5bbe585bcc788366867a21a018a023c
Raid-dev <a_rauf2004@mail.ru>
2024-02-23 15:58:02 +0000

    Added executable program script

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ int main(void)
 
 	n = rand() - RAND_MAX / 2;
 
-	printf("Last digit of %i is %i last_digit", n, (n % 10));
+	printf("Last digit of %i is %i", n, (n % 10));
 
 	if ((n % 10) > 5)
 	{
