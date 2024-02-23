3284829f5a1f9544e5b8f6b9ec7e4a04ea89d2e8
Raid-dev <a_rauf2004@mail.ru>
2024-02-23 16:49:41 +0000

    Added executable program script

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	unsigned int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+
+		i++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
