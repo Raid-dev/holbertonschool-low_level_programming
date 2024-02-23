f69fc720477482ec54fc3037bc39fd8bc1a07de7
Raid-dev <a_rauf2004@mail.ru>
2024-02-23 07:10:32 +0000

    Added executable script

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -8,12 +9,7 @@
 
 int main(void)
 {
-	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	int i = 0;
-	
-	for(i = 0; i < 59; i++)
-		putchar(msg[i]);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }
