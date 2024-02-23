648c1b9e8c27b109e533943dc8e4ce4d1e0d94eb
Raid-dev <a_rauf2004@mail.ru>
2024-02-23 18:14:06 +0000

    Added executable program script

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: ALways 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 9; j++)
+		{
+			for (k = i; k < 10; k++)
+			{
+				for (z = j + 1; z < 10; z++)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(' ');
+					putchar(k + '0');
+					putchar(z + '0');
+				
+					if (i == 9 && j == 8 && k == 9 && z == 9)
+					{
+						putchar('\n');
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+
+	return (0);
+}
