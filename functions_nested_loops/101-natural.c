93e5cf125267a62dca0acb232bd2a6cc0b5f8afb
Raid-dev <a_rauf2004@mail.ru>
2024-02-24 19:59:48 +0000

    Added executable function

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * sum_multiples - Prints sum of multiples
@@ -14,7 +15,7 @@ int sum_multiples(void)
 
 	while (i <= 1024)
 	{
-		if (i % 3 || i % 5)
+		if (i % 3 == 0 || i % 5 == 0)
 		{
 			sum = sum + i;
 		}
@@ -22,5 +23,7 @@ int sum_multiples(void)
 		i++;
 	}
 
-	return (sum);
+	printf(sum);
+
+	_putchar('\n');
 }
