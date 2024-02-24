2ba6e9c4afcfb04da08d88eba029baa67d664aee
Raid-dev <a_rauf2004@mail.ru>
2024-02-24 09:16:02 +0000

    Added executable program script

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-extern int _putchar(char c);
-
 /**
  * main - Entry point
  *
@@ -10,14 +8,7 @@ extern int _putchar(char c);
 
 int main(void)
 {
-	int i;
-	
-	char str[] = "_putchar";
-
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(str[i]);
-	}
+	print_string("_putchar");
 
 	return (0);
 }
