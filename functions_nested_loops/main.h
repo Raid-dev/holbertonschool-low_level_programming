0faff1d8941af25675758ef34cb65c218d555f0d
Raid-dev <a_rauf2004@mail.ru>
2024-02-24 09:34:32 +0000

    Added function script

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -6,23 +6,11 @@
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
- *
- * print_string - prints the string str to stdout
- * @str: The string to print
- *
  */
+
+int _putchar(char c);
 
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
-}
-
-void print_string(char str[], int length)
-{
-	int i;
-
-	for (i = 0; i < length; i++)
-	{
-		_putchar(str[i]);
-	}
 }
