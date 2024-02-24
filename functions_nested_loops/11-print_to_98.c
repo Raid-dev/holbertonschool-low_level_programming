7f0669794ae95b19f262b1e8741b651b202bc606
Raid-dev <a_rauf2004@mail.ru>
2024-02-24 18:39:04 +0000

    Added executable function

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,59 @@
+#include "main.h"
+
+/**
+ * print_to_98 - Prints all natural numbers from n to 9, followed by a new line
+ * @n: The input number
+ *
+ * Return: Numbers
+ */
+
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		int i = 0;
+
+		while (i <= n - 98)
+		{
+			int z = 98 + i;
+
+			_putchar(z / 10 + '0');
+			_putchar(z % 10 + '0');
+			
+			if (z == n)
+			{
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+
+			i++;
+		}
+	}
+	else
+	{
+		int i = 0;
+		
+                while (i <= 98 - n)
+                {
+                        int z = n + i;
+
+                        _putchar(z / 10 + '0');
+                        _putchar(z % 10 + '0');
+			
+			if (z == 98)
+			{
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+
+                        i++;
+                }
+}
