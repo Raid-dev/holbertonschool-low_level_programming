55b513b82fe3d114931c1ad77ef3116ecc119db6
Raid-dev <a_rauf2004@mail.ru>
2024-02-24 15:26:57 +0000

    Added executable function

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _abs - Computes the absolute value of the integer n
+ * @n: The input integer
+ *
+ * Return: Always int (Success)
+ */
+
+int _abs(int n)
+{	
+	return (abs(n));
+}
