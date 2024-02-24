50016ac9af1414a2476442a0678a19b5d28747e2
Raid-dev <a_rauf2004@mail.ru>
2024-02-24 12:44:46 +0000

    Decleared a new function

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,15 +1,29 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
+ * _putchar - writes the character c
  *
- * print_alphabet - prints the alphabet in lowercase to stdout
+ * print_alphabet - prints the alphabet in lowercase
  *
- * print_alphabet_x10 - prints 10 times the alphabet in lowercase to stdout
+ * print_alphabet_x10 - prints 10 times the alphabet in lowercase
  *
- * _islower - checks if the character c is lowercase or not
+ * _islower - checks if the character c is lowercase
  *
  * _isalpha - checks if the character c is a letter
+ *
+ * print_sign - prints the sign of the number n
+ *
+ * _abs - computes the absolute value of the integer n
+ *
+ * print_last_digit - prints the last digit of the number n
+ *
+ * jack_bauer - prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
+ *
+ * times_table - prints the 9 times table, starting with 0
+ *
+ * add - adds two integers a and b, and returns the result
+ *
+ * print_to_98 - prints all natural numbers from n to 98
  */
 
 int _putchar(char c);
@@ -21,3 +35,17 @@ void print_alphabet_x10(void);
 int _islower(int c);
 
 int _isalpha(int c);
+
+int print_sign(int n);
+
+int _abs(int n);
+
+int print_last_digit(int n);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int a, int b);
+
+void print_to_98(int n);
