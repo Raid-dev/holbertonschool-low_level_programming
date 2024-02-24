7859c3ffa10ab8ef1601666a463fc8dd8d472136
Raid-dev <a_rauf2004@mail.ru>
2024-02-24 18:11:31 +0000

    Added executable function

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers a and b
+ * @a - First integer
+ * @b - Second integer
+ *
+ * Result: Always int (Success)
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
