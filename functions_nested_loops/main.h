41688657266a2cc460c7c68a3bf9895018a0e5b6
Raid-dev <a_rauf2004@mail.ru>
2024-02-24 10:26:22 +0000

    Decleared a new function

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -4,12 +4,13 @@
  * _putchar - writes the character c to stdout
  * @c: The character to print
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * print_alphabet - prints the alphabet in lower case to stdout
  *
- * print_alphabet - prints the alphabet to stdout
+ * print_alphabet_x10 - prints 10 times the alphabet in lower case to stdout
  */
 
 int _putchar(char c);
 
 void print_alphabet(void);
+
+void print_alphabet_x10(void);
