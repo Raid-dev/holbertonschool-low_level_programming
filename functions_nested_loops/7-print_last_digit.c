0a9dc4c1a4ddfc19056bfbd219dc8df81930c32f
Raid-dev <a_rauf2004@mail.ru>
2024-02-24 16:41:27 +0000

    Added executable function

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints the last digit of the number n
+ * @n: The input number
+ *
+ * Return: Always 0 (Success)
+ */
+
+int print_last_digit(int n)
+{
+	_putchar((n % 10) + '0');
+
+	return (0);
+}
