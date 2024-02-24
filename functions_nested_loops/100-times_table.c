c66c5c02f9cddb1682a3c40eb052c2a7dd2cbcb3
Raid-dev <a_rauf2004@mail.ru>
2024-02-24 19:43:38 +0000

    Added executable function

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -9,47 +9,42 @@
 
 void print_times_table(int n)
 {
-	if (n <= 15 && n >= 0)
+	int i = 0, j = 0;
+	while (i <= n)
 	{
-		int i;
-		int j;
-
-		for (i = 0; i <= n; i++)
+		if (n > 15 || n < 0)
+			break;
+		while (j <= n)
 		{
-			for(j = 0; j <= n; j++)
+			if (i * j <= 9)
+				_putchar((i * j) % 10 + '0');
+			else if (i * j <= 99)
 			{
-				if (i * j < 10)
-				{
-					_putchar(i * j + '0');
-				}
-				else if (i * j >= 100)
-				{
-					_putchar(i * j / 100);
-					_putchar(i * j / 10 % 10 + '0');
-					_putchar(i * j % 10 + '0');
-				}
-				else
-				{
-					_putchar(i * j / 10);
-					_putchar(i * j % 10 + '0');
-				}
-
-				if (j != n - 1)
-				{
-					_putchar(',');
-                        		_putchar(' ');
-
-					if (i * (j + 1) < 10)
-					{
-						_putchar(' ');
-						_putchar(' ');
-					}
-					else if (i * (j + 1) < 100)
-					{
-						_putchar(' ');
-					}
-				}
+				_putchar((i * j) / 10 + '0');
+				_putchar((i * j) % 10 + '0');
 			}
+			else
+			{
+				_putchar((i * j) / 100 + '0');
+				_putchar(((i * j) / 10) % 10 + '0');
+				_putchar((i * j) % 10 + '0');
+			}
+			if (j != n)
+			{
+				_putchar(',');
+				if (i * (j + 1) <= 9)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else if (i * (j + 1) <= 99)
+					_putchar(' ');
+				_putchar(' ');
+			}
+			j++;
 		}
+		j = 0;
+		i++;
+		_putchar('\n');
 	}
 }
