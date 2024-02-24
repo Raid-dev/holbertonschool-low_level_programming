35b2d1fe31e9202080e000acd743e1d7c739a963
Raid-dev <a_rauf2004@mail.ru>
2024-02-24 18:08:04 +0000

    Added executable function

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -17,23 +17,23 @@ void times_table(void)
 		{
 			if (i * j < 10)
 			{
-			_putchar(i * j + '0');
+				_putchar(i * j + '0');
 			}
 			else
 			{
-			_putchar(i * j / 10 + '0');
-			_putchar(i * j % 10 + '0');
+				_putchar(i * j / 10 + '0');
+				_putchar(i * j % 10 + '0');
 			}
-			
+
 			if (j != 9)
 			{
 				_putchar(',');
 				_putchar(' ');
 
-			if ((i) * (j+1) < 10)
-			{
-				_putchar(' ');
-			}
+				if ((i) * (j + 1) < 10)
+				{
+					_putchar(' ');
+				}
 			}
 		}
 
