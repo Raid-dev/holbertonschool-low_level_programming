ec63be5597ff719150bd6a42650529ad9ce49316
Raid-dev <a_rauf2004@mail.ru>
2024-02-24 10:42:44 +0000

    Added executable function

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _islower - Checks for lowercase character
+ */
+
+int _islower(int c)
+{
+	if (islower(c))
+	{
+		return (1);
+	}
+	
+	return (0);
+}
