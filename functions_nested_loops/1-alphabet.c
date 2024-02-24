bf8a7b0555cab74f29c39274a6effc002fdc55f3
Raid-dev <a_rauf2004@mail.ru>
2024-02-24 10:21:41 +0000

    Added executable function

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n";
+	
+	int i = 0;
+
+	while (alphabet[i])
+	{
+		_putchar(alphabet[i]);
+
+		i++;
+	}
+}
