694eb3731e7110c0ae73b66566580af41c765975
Raid-dev <a_rauf2004@mail.ru>
2024-02-24 09:19:02 +0000

    Added function script

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <string.h>
 
 /**
  * _putchar - writes the character c to stdout
@@ -6,9 +7,25 @@
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
+ *
+ * print_string - prints the string str to stdout
+ * @str: The string to print
+ *
  */
 
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
+}
+
+void print_string(char str)
+{
+	int i;
+	
+	int length = strlen(str);
+
+	for (i = 0; i < length; i++)
+	{
+		_putchar(str[i]);
+	}
 }
