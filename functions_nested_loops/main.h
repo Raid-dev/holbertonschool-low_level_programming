302f87156d410c425f798aba73141658f00c52e1
Raid-dev <a_rauf2004@mail.ru>
2024-02-24 10:44:43 +0000

    Decleared a new function

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -7,6 +7,9 @@
  * print_alphabet - prints the alphabet in lower case to stdout
  *
  * print_alphabet_x10 - prints 10 times the alphabet in lower case to stdout
+ *
+ * _islower - checks if c is lowercase or not
+ * @c: The character to check
  */
 
 int _putchar(char c);
@@ -14,3 +17,5 @@ int _putchar(char c);
 void print_alphabet(void);
 
 void print_alphabet_x10(void);
+
+int _islower(int c);
