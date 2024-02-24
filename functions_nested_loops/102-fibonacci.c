9d561ade92fc3e689e0d088178780db294f27dfd
Raid-dev <a_rauf2004@mail.ru>
2024-02-24 21:18:18 +0000

    Added executable function

--- functions_nested_loops/102-fibonacci.c
+++ functions_nested_loops/102-fibonacci.c
@@ -8,14 +8,14 @@
 
 int main(void)
 {
- 	long int i = 1;
+	long int i = 1;
 	long int j = 2;
 	long int temp;
-	
+
 	int k = 0;
 
 	printf("%ld, ", i);
-        printf("%ld, ", j);
+	printf("%ld, ", j);
 
 	while (k < 48)
 	{
