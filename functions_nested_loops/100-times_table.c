ec8267ce03f12d35c73b2fe930752f6a64532df2
Raid-dev <a_rauf2004@mail.ru>
2024-02-24 19:30:31 +0000

    Added executable function

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -14,9 +14,9 @@ void print_times_table(int n)
 		int i;
 		int j;
 
-		for (i = 0; i < n; i++)
+		for (i = 0; i <= n; i++)
 		{
-			for(j = 0; j < n; j++)
+			for(j = 0; j <= n; j++)
 			{
 				if (i * j < 10)
 				{
@@ -25,7 +25,7 @@ void print_times_table(int n)
 				else if (i * j >= 100)
 				{
 					_putchar(i * j / 100);
-					_putchar(i * j / 10);
+					_putchar(i * j / 10 % 10 + '0');
 					_putchar(i * j % 10 + '0');
 				}
 				else
@@ -40,6 +40,11 @@ void print_times_table(int n)
                         		_putchar(' ');
 
 					if (i * (j + 1) < 10)
+					{
+						_putchar(' ');
+						_putchar(' ');
+					}
+					else if (i * (j + 1) < 100)
 					{
 						_putchar(' ');
 					}
