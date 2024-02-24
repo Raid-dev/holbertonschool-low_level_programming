42081b934669c679651ada59ea3e09221b30a689
Raid-dev <a_rauf2004@mail.ru>
2024-02-24 17:14:24 +0000

    Added executable function

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
+ *
+ * Return: Text (Success)
+ */
+
+void jack_bauer(void)
+{
+	int i;
+	int j;
+	int k;
+	int z;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
+			for (k = 0; k < 6; k++)
+			{
+				for (z = 0; z < 10; z++)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+
+					putchar(':');
+
+					putchar(k + '0');
+					putchar(z + '0');
+
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
