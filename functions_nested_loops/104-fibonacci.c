356d88aba56f17d89aed8669f5daeedb723c1c6d
Raid-dev <a_rauf2004@mail.ru>
2024-02-25 13:01:39 +0000

    Added executable script

---
+++ functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	long int i = 1;
+	long int j = 2;
+	long int temp;
+
+	int k = 0;
+
+	printf("%ld, ", i);
+	printf("%ld, ", j);
+
+	while (k < 96)
+	{
+		temp = i + j;
+
+		printf("%ld", temp);
+
+		i = j;
+
+		if (k != 95)
+			printf(", ");
+
+		j = temp;
+
+		k++;
+	}
+
+	printf("\n");
+
+	return (0);
+}
