eadbe7f25ce022e4354f67f4a35d4cfb015393bc
Raid-dev <a_rauf2004@mail.ru>
2024-02-26 07:23:57 +0000

    Added executable function

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <ctype>
+
+/**
+ * _isdigit - Checks for a digit for the character c
+ * @c: The input character
+ *
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (isdigit(c))
+		return (1);
+
+	return (0);
+}
