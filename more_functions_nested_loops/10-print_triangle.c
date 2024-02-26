2672a0205e97aaf97419fd48d10afbc94d135cfd
Raid-dev <a_rauf2004@mail.ru>
2024-02-26 10:33:18 +0000

    Added executable function

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_triangle - Entry point
+ *
+ * Description: Prints a triangle of size size
+ * @size: The input size
+ */
+
+void print_triangle(int size)
+{
+	if (size > 0)
+	{
+		int i;
+		int j;
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < i - 1; j++)
+				_putchar(' ');
+
+			_putchar('#');
+
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
