bcf354471731c9a50cd428dd77ad52876a720ab0
Raid-dev <a_rauf2004@mail.ru>
2024-02-26 07:30:13 +0000

    Added executable function

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two integers a and b
+ * @a: The first input number
+ * @b: The second input number
+ *
+ * Return: Multiplication of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
