2e1c83be91be9a076a342b6896fa17b785844819
Raid-dev <a_rauf2004@mail.ru>
2024-03-02 19:48:46 +0000

    Added executable function

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -11,11 +11,11 @@ void print_number(int n)
 {
 	if (n < 0)
 	{
-        	_putchar('-');
-        	n = -n;
-    	}
+		_putchar('-');
+		n = -n;
+	}
 
-	if (n / 10)
+	if (n / 10 != 0)
 	{
 		print_number(n / 10);
 	}
