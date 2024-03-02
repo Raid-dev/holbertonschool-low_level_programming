48d6e92741f1c7be9d846cd61be0ebcf81a3c8dd
Raid-dev <a_rauf2004@mail.ru>
2024-03-02 21:55:12 +0000

    Added executable function

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -1,13 +1,42 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-void print_number(int n) {
-    if (n < 0) {
-        _putchar('-');
-        n = -n;
-    }
-    if (n / 10) {
-        print_number(n / 10);
-    }
-    _putchar(n % 10 + '0');
+/**
+ * print_number - Entry point
+ *
+ * Description: Prints the integer n
+ * @n: The input integer
+ */
+
+void print_number(int n)
+{
+	char buffer[20];
+	int index;
+	int i;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	index = 0;
+
+	while (n > 0)
+	{
+		buffer[index++] = '0' + (n % 10);
+		n /= 10;
+	}
+
+	for (i = index - 1; i >= 0; i--)
+	{
+		_putchar(buffer[i]);
+	}
 }
-
