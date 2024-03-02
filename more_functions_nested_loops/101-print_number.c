a48c847b99be8a8afab1d9d5bfd125afd2a8c711
Raid-dev <a_rauf2004@mail.ru>
2024-03-02 20:02:54 +0000

    Added executable function

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -11,13 +11,30 @@
 
 void print_number(int n)
 {
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
 	char buffer[20];
+	int index = 0;
+
+	while (n > 0)
+	{
+		buffer[index++] = '0' + (n % 10);
+		n /= 10;
+	}
 
 	int i;
 
-	int length = sprintf(buffer, "%d", n);
-
-	for (i = 0; i < length; i++)
+	for (i = index - 1; i >= 0; i--)
 	{
 		_putchar(buffer[i]);
 	}
