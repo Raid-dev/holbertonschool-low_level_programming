09d15b727eeec529d1577d60401db00c7868f3ec
Raid-dev <a_rauf2004@mail.ru>
2024-03-03 17:06:52 +0000

    Added executable function

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - Entry point
+ *
+ * Description: Reverses the content of the array of integers a in size of n
+ * @a: The input array
+ * @n: The number of elements
+ */
+
+void reverse_array(int *a, int n)
+{
+	int temp[n] = *a;
+
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		a[i] = temp[n - i - 1];
+	}
+}
