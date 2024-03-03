b0a00087d6badda10f1e523bea182425c58dd90a
Raid-dev <a_rauf2004@mail.ru>
2024-03-03 12:26:59 +0000

    Added executable function

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcpy - Entry point
+ *
+ * Description: Copies the pointer to src to the buffer pointer to dest
+ * @dest: The input pointer
+ * @src: The input pointer
+ *
+ * Return: The pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+	int j;
+
+	while (src[i])
+		i++;
+
+	for (j = 0; j < i; j++)
+		dest[i] = src[i];
+
+	return (*dest);
+}
