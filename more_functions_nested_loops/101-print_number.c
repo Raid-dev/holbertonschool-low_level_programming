1d6ac7a6a16ab5daaa1ac913e9d04472a1e4449e
Raid-dev <a_rauf2004@mail.ru>
2024-03-03 19:21:38 +0000

    Added executable function

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -1,1 +1,26 @@
+#include "main.h"
 
+/**
+ * print_number - Entry point
+ *
+ * Description: Prints the integer n
+ * @n: The input integer
+ */
+
+void print_number(int n)
+{
+	unsigned int k = n;
+
+	if (n < 0)
+	{
+
+		_putchar('-');
+
+		k = -k;
+	}
+
+	if ((k / 10) > 0)
+		print_number(k / 10);
+
+	_putchar((k % 10) + '0');
+}
