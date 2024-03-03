7d81105a48b67cc2d001bcae17d53176146e2083
Raid-dev <a_rauf2004@mail.ru>
2024-03-03 01:20:02 +0000

    Added executable function

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry point
+ *
+ * Description: Updates the value the pointer n points to 98
+ * @n: The input pointer
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
