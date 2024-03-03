0268cb9592c39f4cbc156a36a2e9dfbe3c43e349
Raid-dev <a_rauf2004@mail.ru>
2024-03-03 10:03:16 +0000

    Added executable function

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -11,17 +11,15 @@ void rev_string(char *s)
 {
 	int length = 0;
 	int i;
-	int temp;
+	char temp = *s;
 
 	while (s[length])
 		length++;
 
 	for (i = length - 1; i >= 0; i--)
 	{
-		temp = s[length - i - 1];
-
-		s[length - i - 1] = s[i];
-
-		s[i] = temp;
+		temp[length - i - 1] = s[i];
 	}
+
+	*s = *temp;
 }
