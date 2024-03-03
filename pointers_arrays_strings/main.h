bdeb37ba63b49521c023e250db8c5f582e9e9ebf
Raid-dev <a_rauf2004@mail.ru>
2024-03-03 15:47:19 +0000

    Decleared new functions

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -21,9 +21,25 @@
  *
  * print_array - Prints n elements of an array of integers
  *
- * *_strcpy - Copies the string pointed to by src,including the terminating null byte (\0), to the buffer pointed to by dest
+ * _strcpy - Copies a string pointed to by src,including the terminating null byte (\0), to a buffer pointed to by dest
  *
  * _atoi - Converts a string to an integer
+ *
+ * _strcat - Concatenates two strings
+ *
+ * _strncat - Concatenates two strings but uses at most n bytes from src
+ *
+ * _strncpy - Copies a string
+ *
+ * _strcmp - Compares two strings
+ *
+ * reverse_array - Reverses the content of an array of integers
+ *
+ * string_toupper - Changes all lowercase letters of a string to uppercase
+ *
+ * cap_string - Capitalizes all words of a string
+ *
+ * leet - Encodes a string into 1337
  */
 
 int _putchar(char c);
@@ -49,3 +65,19 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
 int _atoi(char *s);
+
+char *_strcat(char *dest, char *src);
+
+char *_strncat(char *dest, char *src, int n);
+
+char *_strncpy(char *dest, char *src, int n);
+
+int _strcmp(char *s1, char *s2);
+
+void reverse_array(int *a, int n);
+
+char *string_toupper(char *);
+
+char *cap_string(char *);
+
+char *leet(char *);
