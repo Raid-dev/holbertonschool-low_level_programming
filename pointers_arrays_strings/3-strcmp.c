0f10a1ad1d3ffbabc044cce1a096f5ef458f8920
Raid-dev <a_rauf2004@mail.ru>
2024-03-03 16:56:15 +0000

    Added executable function

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * _strcpy - Entry point
+ *
+ * Description: Compares two pointer strings s1 and s2
+ * @s1: The first pointer string
+ * @s2: The second pointer string
+ *
+ * Return: 0 if same, -15 if s2 is greater than s1 otherwise 15
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	int j = 0;
+	int k;
+
+	while (s1[i])
+		i++;
+
+	while (s2[j])
+		j++;
+
+	if (i == j)
+	{
+		for (k = 0; k < i; k++)
+		{
+			if (dest[k] != src[k])
+				return (15);
+		}
+
+		return (0);
+	}
+	else if (i > j)
+	{
+		return (15);
+	}
+	else
+	{
+		return (-15);
+	}
+}
