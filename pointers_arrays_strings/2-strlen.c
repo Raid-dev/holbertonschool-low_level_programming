6ad12bfd378808dcd071c960aec1b7dfd8477895
Raid-dev <a_rauf2004@mail.ru>
2024-03-03 08:39:11 +0000

    Added executable function

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - Entry point
+ *
+ * Description: Returns the length of the pointer string s
+ * @s: The input pointer
+ *
+ * Return: The length of the string
+ */
+
+int _strlen(char *s)
+{
+	int length;
+
+	length = strlen(s);
+
+	return (length);
+}
