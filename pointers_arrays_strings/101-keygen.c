4bd056fa8de0db6b167e69a901963f3fb195b7c5
Raid-dev <a_rauf2004@mail.ru>
2024-03-03 13:09:57 +0000

    Added executable function

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -10,13 +10,15 @@
 
 int main(void)
 {
-	char password[10];
+	char password[15];
 	int i;
 
 	srand(time(0));
 
 	for (i = 0; i < 10; i++)
 		password[i] = rand();
+
+	password = "Tada! Congrats";
 
 	printf("%s\n", password);
 
