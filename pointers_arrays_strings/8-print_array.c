b47860556bc14e9f6f29fa961c876450ef683bdc
Raid-dev <a_rauf2004@mail.ru>
2024-03-03 11:57:45 +0000

    Added executable function

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - Entry point
+ *
+ * Description: Prints n elements of the pointer integer array a
+ * @a: The input pointer
+ * @n: The input number
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", *(a + i));
+
+		if (i != n - 1)
+			printf(", ");
+		else
+			_putchar('\n');
+	}
+}
