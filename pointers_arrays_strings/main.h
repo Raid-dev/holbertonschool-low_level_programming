3885d1c0094d6801d03afcf85d409a6673fcfa54
Raid-dev <a_rauf2004@mail.ru>
2024-03-09 23:41:24 +0000

    Decleared new functions

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -48,6 +48,22 @@
  * infinite_add - Adds two numbers
  *
  * print_buffer - Prints buffer
+ *
+ * _memset - Fills memory with a constant byte
+ *
+ * _memcpy - Copies memory area
+ *
+ * _strchr - Locates a character in a string
+ *
+ * _strspn - Gets the length of a prefix substring
+ *
+ * _strpbrk - Searches a string for any of a set of bytes
+ *
+ * _strstr - Locates a substring
+ *
+ * print_chessboard - Prints the chessboard
+ *
+ * print_diagsums - Prints the sum of the two diagonals of a square matrix of integers
  */
 
 int _putchar(char c);
@@ -97,3 +113,19 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 void print_buffer(char *b, int size);
+
+char *_memset(char *s, char b, unsigned int n);
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+char *_strchr(char *s, char c);
+
+unsigned int _strspn(char *s, char *accept);
+
+char *_strpbrk(char *s, char *accept);
+
+char *_strstr(char *haystack, char *needle);
+
+void print_chessboard(char (*a)[8]);
+
+void print_diagsums(int *a, int size);
