d95249f2da568f9163c148d9b38beb84e9bb09c3
Raid-dev <a_rauf2004@mail.ru>
2024-03-10 14:33:07 +0000

    Added executable function

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints a string in reverse
+ *
+ * Description: Prints the string s in reverse
+ * @s: The string
+ *
+ * Return: Nothing.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_puts_recursion(s + 1);
+
+		putchar(*s);
+	}
+}
