f98e6dee286cfad7d471d13c7c2879265cd097ff
Raid-dev <a_rauf2004@mail.ru>
2024-03-10 13:49:41 +0000

    Decleared new functions

---
+++ recursion/main.h
@@ -0,0 +1,36 @@
+#include <unistd.h>
+
+/**
+ * _putchar - Writes the character c
+ *
+ * _puts_recursion - Prints a string
+ *
+ * _print_rev_recursion - Prints a string in reverse
+ *
+ * _strlen_recursion - Returns the length of a string
+ *
+ * factorial - Returns the factorial of a given number
+ *
+ * _pow_recursion - Returns the value of x raised to the power of y
+ *
+ * _sqrt_recursion - Returns the natural square root of a number
+ *
+ * is_prime_number - Returns 1 if the input integer is a prime number,
+ * otherwise return 0
+ */
+
+int _putchar(char c);
+
+void _puts_recursion(char *s);
+
+void _print_rev_recursion(char *s);
+
+int _strlen_recursion(char *s);
+
+int factorial(int n);
+
+int _pow_recursion(int x, int y);
+
+int _sqrt_recursion(int n);
+
+int is_prime_number(int n);
