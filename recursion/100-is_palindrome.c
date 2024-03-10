f536e4b022939de022a7bc1040e7f7deaaecf911
Raid-dev <a_rauf2004@mail.ru>
2024-03-10 16:13:06 +0000

    Added executable script

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -1,4 +1,27 @@
 #include "main.h"
+#include <string.h>
+
+/**
+ * palindrome_checker - Checks the palindrome using recursion
+ *
+ * Description: Checks the palindrome s using recursion
+ * @s: The string
+ * @i: The integer
+ * @n: The integer
+ *
+ * Return: 1 if a string is a palindrome, otherwise 0.
+ */
+
+int palindrome_checker(char *s, int i, int n)
+{
+	if (i == n)
+		return (1);
+	else if (*(s + i) != *(s + n))
+		return (0);
+	else if (i <= n)
+		return (palindrome_checker(s, i + 1, n - 1));
+	return (1);
+}
 
 /**
  * is_palindrome - Returns 1 if a string is a palindrome and 0 if not
@@ -6,21 +29,15 @@
  * Description: Returns 1 if the string s is a palindrome and 0 if not
  * @s: The string
  *
- * Return: 1 if a string is a palindrome, otherwise 0.
+ * Return: Result from palindrome_checker.
  */
 
 int is_palindrome(char *s)
 {
-	int l = 0, i;
+	int size = strlen(s);
 
-	while (s[l])
-		l++;
+	if (size == 0)
+		return (1);
 
-	for (i = 0; i < l / 2; i++)
-	{
-		if (s[i] != s[l - i - 1])
-		return (0);
-	}
-
-	return (1);
+	return (polyndrome_checker(s, 0, size - 1));
 }
