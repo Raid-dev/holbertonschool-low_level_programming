92d66c9107690774928d8575f70e4460078d0482
Raid-dev <a_rauf2004@mail.ru>
2024-03-16 16:19:17 +0000

    Added executable script

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - Returns a pointer to a newly allocated space in memory, which
+ * contains a copy of the string given as a parameter
+ *
+ * Description: Returns a pointer to a newly allocated space in memory, which
+ * contains a copy of the string given as a parameter str
+ * @str: The string to copy
+ *
+ * Return: NULL if str is NULL or duplication fails, a pointer to a new string
+ * which is a duplicate otherwise.
+ */
+
+char *_strdup(char *str)
+{
+	unsigned int i, len = 0;
+	char *dup;
+
+	if (str == NULL)
+		return (NULL);
+
+	dup = malloc(sizeof(str));
+
+	if (dup == NULL)
+		return (NULL);
+
+	while (str[len] != '\0')
+		len++;
+
+	for (i = 0; i < len; i++)
+		dup[i] = str[i];
+
+	dup[len] = '\0';
+
+	return (dup);
+}
