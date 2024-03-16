c7aa5925de483f2659c9e21a3ab9b514c05afc47
Raid-dev <a_rauf2004@mail.ru>
2024-03-16 12:58:51 +0000

    Added executable script

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -21,14 +21,12 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	arr = malloc(size + 1);
+	arr = malloc(size);
 
 	if (arr == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
 		arr[i] = c;
-
-	arr[size] = '\0';
 
 	return (arr);}
