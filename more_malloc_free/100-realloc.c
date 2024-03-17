ad5bede72447cca54769157952aea08319c9ff8d
Raid-dev <a_rauf2004@mail.ru>
2024-03-17 18:39:18 +0000

    Added executable script

--- more_malloc_free/100-realloc.c
+++ more_malloc_free/100-realloc.c
@@ -45,7 +45,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 
 	for (i = 0; i < old_size; i++)
-		(char *)new_ptr[i] = (char *)ptr[i];
+		new_ptr[i] = ((char *)ptr)[i];
+
+	free(ptr);
 
 	return (new_ptr);
 }
