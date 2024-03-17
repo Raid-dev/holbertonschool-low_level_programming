b724ad99388893d0cc50e353e429e43ad75c4aa5
Raid-dev <a_rauf2004@mail.ru>
2024-03-17 19:12:15 +0000

    Added executable script

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -4,16 +4,6 @@
 #include <string.h>
 
 /**
- * errors - handles errors for main
- */
-
-void errors(void)
-{
-	printf("Error\n");
-	exit(98);
-}
-
-/**
  * main - Multiplies two positive numbers
  *
  * Description: Multiplies two arguments of the program
@@ -25,44 +15,30 @@ void errors(void)
 
 int main(int argc, char *argv[])
 {
-	char *s1, *s2;
-	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+	long long int num1, num2;
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit (98);
+	}
 
-	s1 = argv[1], s2 = argv[2];
-	if (argc != 3 || !isdigit(s1) || !isdigit(s2))
-		errors();
-	len1 = strlen(s1);
-	len2 = strlen(s2);
-	len = len1 + len2 + 1;
-	result = malloc(sizeof(int) * len);
-	if (!result)
-		return (1);
-	for (i = 0; i <= len1 + len2; i++)
-		result[i] = 0;
-	for (len1 = len1 - 1; len1 >= 0; len1--)
+	for (int i = 1; i < argc; i++)
 	{
-		digit1 = s1[len1] - '0';
-		carry = 0;
-		for (len2 = strlen(s2) - 1; len2 >= 0; len2--)
+		for (int j = 0; argv[i][j] != '\0'; j++)
 		{
-			digit2 = s2[len2] - '0';
-			carry += result[len1 + len2 + 1] + (digit1 * digit2);
-			result[len1 + len2 + 1] = carry % 10;
-			carry /= 10;
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				exit (98);
+			}
 		}
-		if (carry > 0)
-			result[len1 + len2 + 1] += carry;
 	}
-	for (i = 0; i < len - 1; i++)
-	{
-		if (result[i])
-			a = 1;
-		if (a)
-			putchar(result[i] + '0');
-	}
-	if (!a)
-		putchar('0');
-	putchar('\n');
-	free(result);
+
+	num1 = atol(argv[1]);
+	num2 = atol(argv[2]);
+
+	printf("%ld\n", num1 * num2);
+
 	return (0);
 }
+
