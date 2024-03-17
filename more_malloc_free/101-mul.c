ff8ff5e5dc03171bd5c3f12085ca2a26ad897915
Raid-dev <a_rauf2004@mail.ru>
2024-03-17 18:59:45 +0000

    Added executable script

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 
 /**
  * main - Multiplies two positive numbers
@@ -12,8 +13,11 @@
  * Return: Multiplication of two numbers.
  */
 
-int main(int ac, char **av)
+int main(int ac, char *av[])
 {
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+
 	if (ac != 3 || !isdigit(av[1]) || !isdigit(av[2]))
 	{
 		printf("Error\n");
@@ -21,5 +25,51 @@ int main(int ac, char **av)
 		exit(98);
 	}
 
-	return (((int *)av)[1] * ((int *)av)[2]);
+	s1 = argv[1], s2 = argv[2];
+
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+
+	result = malloc(sizeof(int) * len);
+
+	if (!result)
+		return (NULL);
+
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+
+	for (len1 = len1 - 1; len1 >= 0; len1--)
+	{
+		digit1 = s1[len1] - '0';
+		carry = 0;
+
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
+	}
+
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+
+	if (!a)
+		_putchar('0');
+
+	_putchar('\n');
+
+	free(result);
+
+	return (0);
 }
