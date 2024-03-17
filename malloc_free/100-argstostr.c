8b164b310553abc80577c5eb83e5da1a2881498b
Raid-dev <a_rauf2004@mail.ru>
2024-03-17 12:10:43 +0000

    Added executable script

--- malloc_free/100-argstostr.c
+++ malloc_free/100-argstostr.c
@@ -24,34 +24,27 @@ char *argstostr(int ac, char **av)
 	{
 		if ((int *)av[i] != NULL)
 		{
-			while (*av[i] != '\0')
+			while (av[i][arg_len] != '\0')
 				arg_len++;
 		}
 	}
-	str = malloc(arg_len + ac * 2);
+
+	str = malloc(arg_len + ac + 1);
+
 	if (str == NULL)
 		return (NULL);
-	arg_len = 0;
 
 	for (i = 1; i < ac + 1; i++)
 	{
-		while (*str[str_len] != '\0')
-			str_len++;
-
 		if ((int *)av[i] != NULL)
 		{
-			while (*av[i] != '\0')
-				arg_len++;
+			for (j = 0; av[i][j] != '\0'; j++, str_len++)
+				str[str_len] = av[i][j];
+			str[str_len++] = '\n';
 		}
-
-		for (j = str_len; j < str_len + arg_len; j++)
-			*str[j] = *av[j - str_len];
-
-		*str[str_len + arg_len] = '\0';
-		*str[str_len + arg_len + 1] = '\n';
-
-		arg_len = 0;
 	}
+
+	str[str_len] = '\0';
 
 	return (*str);
 }
