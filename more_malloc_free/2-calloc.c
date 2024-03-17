fa266a49ab6a89be2cf40d6690c43d15a1eb10fe
Raid-dev <a_rauf2004@mail.ru>
2024-03-17 17:07:18 +0000

    Added executable scriptA

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -16,23 +16,8 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	int **arr;
-	unsigned int i;
 
-	if (nmemb == 0 || size == 0)
-		return (NULL);
-
-	arr = calloc(nmemb);
-
-	if (arr == NULL)
-		return (NULL);
-
-	for (i = 0; i < nmemb; i++)
-	{
-		arr[i] = calloc(size);
-
-		if (arr[i] == NULL)
-			return (NULL);
-	}
+	arr = calloc(nmemb, size);
 
 	return (arr);
 }
