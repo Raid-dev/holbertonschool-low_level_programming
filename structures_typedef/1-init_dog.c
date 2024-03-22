669139af1fdc52faa749af763a956444249f503a
Raid-dev <a_rauf2004@mail.ru>
2024-03-22 15:03:47 +0000

    Added executable script

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * init_dog - Initializes variables of type struct dog
+ *
+ * Description: Initializes the variables name, age, owner of type struct dog d
+ * @d: The type of struct dog
+ * @name: The name of dog
+ * @age: The age of dog
+ * @owner: The owner of dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
