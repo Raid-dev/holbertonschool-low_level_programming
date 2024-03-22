658993806514975ce12afd792fca50c434035731
Raid-dev <a_rauf2004@mail.ru>
2024-03-22 18:57:14 +0000

    Added executable script

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -3,6 +3,33 @@
 #include <stdlib.h>
 
 /**
+ * copy_str - Copies a string
+ *
+ * Description: Copies the string str
+ * @str: The string to copy
+ *
+ * Return: NULL on failure, a copy of a string otherwise.
+ */
+
+char *copy_str(char *str)
+{
+	unsigned int i = 0, j = 0;
+
+	while (str[i] != '\0')
+		i++;
+
+	char *copy = malloc((i + 1) * sizeof(char));
+
+	if (copy == NULL)
+		return (NULL);
+
+	for (j = 0; j <= i; j++)
+		copy[j] = str[j];
+
+	return (copy);
+}
+
+/**
  * new_dog - Creates a new dog
  *
  * Description: Creates a new dog with parameters name, age, owner
@@ -17,17 +44,31 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
 
-	d = malloc(sizeof(struct dog_t));
+	d = malloc(sizeof(dog_t));
 
 	if (d == NULL)
+		return (NULL);
+
+	d->name = str_cpy(name);
+
+	if (d->name == NULL)
 	{
 		free(d);
+
 		return (NULL);
 	}
 
-	d->name = name;
+	d->owner = str_cpy(owner);
+
+	if (d->owner == NULL)
+	{
+		free(d->name);
+		free(d);
+
+		return (NULL);
+	}
+
 	d->age = age;
-	d->owner = owner;
 
 	return (d);
 }
