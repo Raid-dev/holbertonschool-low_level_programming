ac27d178e05423840c4aca3da4a52f4c53c93735
Raid-dev <a_rauf2004@mail.ru>
2024-03-22 17:58:57 +0000

    Added executable script

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,30 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * new_dog - Creates a new dog
+ *
+ * Description: Creates a new dog with parameters name, age, owner
+ * @name: The name of dog
+ * @age: The age of dog
+ * @owner: The owner of dog
+ *
+ * Return: NULL on failure, a pointer to new dog type otherwise.
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog *d;
+
+	d = malloc(sizeof(struct dog));
+
+	if (d == NULL)
+		return (NULL);
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+
+	return (d);
+}
