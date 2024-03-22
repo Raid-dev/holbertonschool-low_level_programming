7191709774a8d3757daa6371633bbdf5ae90379b
Raid-dev <a_rauf2004@mail.ru>
2024-03-22 14:42:45 +0000

    Decleared new structure and functions

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * dog - Type structure
+ *
+ * Description: Dog type structure
+ * @name: The name of dogs
+ * @age: The age of dogs
+ * @owner: The owner of dogs
+ */
+
 struct dog
 {
 	char *name;
