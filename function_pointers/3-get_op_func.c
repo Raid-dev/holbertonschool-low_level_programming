ff9d94a75818db9c8ee849320cd2fb4a77230ca9
Raid-dev <a_rauf2004@mail.ru>
2024-03-23 22:48:53 +0000

    Added executable script

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,36 @@
+#include "calc.h"
+#include <stdio.h>
+
+/**
+ * get_op_func - Selects the correct function to perform the operation
+ *
+ * Description: Selects the correct function to perform the operation s
+ * @s: The operation to be performed
+ *
+ * Return: A pointer to function.
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
+
+	int i;
+
+	i = 0;
+
+	while (i < 5)
+	{
+		if (ops[i][0] == s)
+			return (ops[i]1);
+		i++;
+	}
+
+	return (NULL);
+}
