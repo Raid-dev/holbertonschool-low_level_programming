ed8bb6ad6a73b6259d423834e5175440bee0d4e7
Raid-dev <a_rauf2004@mail.ru>
2024-03-23 22:28:46 +0000

    Added executable script

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,77 @@
+#include "calc.h"
+#include <stdio.h>
+
+/**
+ * op_add - Adds parameters
+ *
+ * Description: Adds the parameters a and b
+ * @a: The first parameter
+ * @b: The second parameter
+ *
+ * Return: Sum of parameters.
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Subtracts parameters
+ *
+ * Description: Subtracts the parameters a and b
+ * @a: The first parameter
+ * @b: The second parameter
+ *
+ * Return: Subtraction of parameters.
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Multiplies parameters
+ *
+ * Description: Multiplies the parameters a and b
+ * @a: The first parameter
+ * @b: The second parameter
+ *
+ * Return: Multiplication of parameters.
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Divides parameters
+ *
+ * Description: Divides the parameters a and b
+ * @a: The first parameter
+ * @b: The second parameter
+ *
+ * Return: Division of parameters.
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Calculates modulo of parameters
+ *
+ * Description: Calculates modulo of the parameters a and b
+ * @a: The first parameter
+ * @b: The second parameter
+ *
+ * Return: Modulo of parameters.
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
