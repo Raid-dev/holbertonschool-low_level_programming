e6a35cddccec230f3d6ea9a52c44c1225aa0f7c4
Raid-dev <a_rauf2004@mail.ru>
2024-03-23 13:23:40 +0000

    Decleared new functions

---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,41 @@
+#include <unistd.h>
+
+/**
+ * _putchar - Writes the character c
+ *
+ * print_name - Prints a name
+ *
+ * array_iterator - Executes a function given as a parameter on each element of an array
+ *
+ * int_index - Searches for an integer
+ *
+ * op_add - Adds parameters
+ *
+ * op_sub - Subtracts parameters
+ *
+ * op_mul - Multiplies parameters
+ *
+ * op_div - Divides parameters
+ *
+ * op_mod - Calculates remainder of the division of parameters
+ */
+
+int _putchar(char c);
+
+void print_name(char *name, void (*f)(char *));
+
+void array_iterator(int *array, size_t size, void (*action)(int));
+
+int int_index(int *array, int size, int (*cmp)(int));
+
+int op_add(int a, int b);
+
+int op_sub(int a, int b);
+
+int op_mul(int a, int b);
+
+int op_div(int a, int b);
+
+int op_mod(int a, int b);
+
+int (*get_op_func(char *s))(int, int);
