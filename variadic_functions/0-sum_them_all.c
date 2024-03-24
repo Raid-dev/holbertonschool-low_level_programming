d1d99ceb90e76b77504e20f9ae361e78531ada64
Raid-dev <a_rauf2004@mail.ru>
2024-03-24 12:39:36 +0000

    Decleared new functions

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - Returns the sum of all its parameters
+ *
+ * Description: Returns the sum of  all its parameters starting from n
+ * @n: The fixed argument
+ *
+ * Return: 0 if n is 0, the sum of all its parameters otherwise.
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	if (n == 0)
+		return (0);
+
+	unsigned int i;
+	int sum = 0;
+
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(args, int);
+
+	va_end(args);
+
+	return (sum);
+}
