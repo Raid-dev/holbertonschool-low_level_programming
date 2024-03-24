d85218e197b8f9f3bea221bc980da729506f1a21
Raid-dev <a_rauf2004@mail.ru>
2024-03-24 17:34:01 +0000

    Added executable script

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,9 +1,18 @@
-global	main
-	extern	printf
+section .data
+    hello db 'Hello, World', 0xA ; Print
+
+section .text
+    global main
+
 main:
-	mov edi, format
-	xor eax, eax
-	call printf
-	mov eax, 0
-	ret
-format: db `Hello, World\n`, 0
+    ; Write the string to stdout
+    mov rax, 1              ; The system call for sys_write
+    mov rdi, 1              ; File descriptor 1 is stdout
+    mov rsi, hello          ; Address of the string to output
+    mov rdx, 13             ; The number of bytes to write
+    syscall                 ; Invoke the kernel
+
+    ; Exit the program
+    mov rax, 60             ; The system call for sys_exit
+    xor rdi, rdi            ; Return a code of 0
+    syscall                 ; Invoke the kernel
