46070ca9f4e3dc24054fbffe1034fd2c0090915b
Raid-dev <a_rauf2004@mail.ru>
2024-04-06 08:52:57 +0000

    Added executable script

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - Prints the list_t list h and returns the number of elements
+ * @h: The list_lt list to print
+ *
+ * Return: The number of elements.
+ */
+
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)");
+		else
+			printf("[%i] %s \n", h->len);
+
+		h = h->next;
+
+		i++;
+	}
+
+	return (i);
+}
