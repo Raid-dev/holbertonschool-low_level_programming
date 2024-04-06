7f9189cd63bd14e60e956d9d573a59e142b1e8c5
Raid-dev <a_rauf2004@mail.ru>
2024-04-06 10:08:21 +0000

    Added executable script

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * _strlen - Returns the length of the string str
+ * @str: The string
+ *
+ * Return: The length.
+ */
+
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+		i++;
+
+	return (i);
+}
+
+/**
+ * add_node - Adds a new node at the beginning of the list_t list head
+ * @head: The list_t list to add to
+ * @str: The string to duplicate
+ *
+ * Return: The node.
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *node = (list_t *)malloc(sizeof(list_t));
+
+	if (node == NULL)
+		return (NULL);
+
+	node->str = strdup(str);
+	node->len = _strlen(str);
+	node->next = *head;
+
+	*head = node;
+
+	return (*head);
+}
