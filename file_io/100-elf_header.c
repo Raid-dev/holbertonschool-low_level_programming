23493ddd7365660783b4af060d5e7a42fe96d077
Raid-dev <a_rauf2004@mail.ru>
2024-04-07 19:34:42 +0000

    Added executable script

--- file_io/100-elf_header.c
+++ file_io/100-elf_header.c
@@ -284,6 +284,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	Elf64_Ehdr *header;
 	int o, r;
 	o = open(argv[1], O_RDONLY);
+
 	if (o == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read file %s\n",
