4f904cd969a37471cc68cb975def86f62cd7e9e0
Raid-dev <a_rauf2004@mail.ru>
2024-04-07 09:55:08 +0000

    Added executable script

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -21,7 +21,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	node->prev = NULL;
 	node->next = *head;
 
+	if (*head != NULL)
+		(*head)->prev = node;
+
 	*head = node;
 
-	return (*head);
+	return (node);
 }
