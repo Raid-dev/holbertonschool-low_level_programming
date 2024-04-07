5e8a812e088674251dca777af95c380ed9ed83c6
Raid-dev <a_rauf2004@mail.ru>
2024-04-07 10:13:17 +0000

    Added executable script

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * add_dnodeint_end - Adds a new node at the end of the dlistint_t list head
+ * @head: The dlistint_t list to add to
+ * @n: The integer to assign
+ *
+ * Return: The new node.
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *node = (dlistint_t *)malloc(sizeof(dlistint_t));
+
+	if (node == NULL)
+		return (NULL);
+
+	node->n = n;
+	node->prev = NULL;
+	node->next = *head;
+
+	if (*head == NULL)
+		*head = node;
+	else
+	{
+		while ((*head)->next != NULL)
+			*head = (*head)->next;
+
+		(*head)->next = node;
+	}
+
+	return (node);
+}
