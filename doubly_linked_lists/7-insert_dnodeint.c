d5f957358857791bca4a67ae0b530ed4b6eaee03
Raid-dev <a_rauf2004@mail.ru>
2024-04-07 13:19:36 +0000

    Added executable script

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -3,6 +3,26 @@
 #include <stdlib.h>
 
 /**
+ * dlistint_len - Returns the number of elements in a linked dlistint_t list h
+ * @h: The dlistint_t list to return its length
+ *
+ * Return: The number of elements.
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+
+	return (i);
+}
+
+/**
  * add_dnodeint - Adds a new node at the beginning of the dlistint_t list head
  * @head: The dlistint_t list to add to
  * @n: The integer of the new node
