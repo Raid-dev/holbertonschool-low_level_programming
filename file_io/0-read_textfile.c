316cbc4a937022e526e7af9dbdb143a31f63a7e6
Raid-dev <a_rauf2004@mail.ru>
2024-04-07 18:58:20 +0000

    Added executable script

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* read_textfile- Read text file print to STDOUT.
+* @filename: text file being read
+* @letters: number of letters to be read
+*
+* Return: The w- actual number of bytes read and printed
+* 0 when function fails or filename is NULL.
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	ssize_t fd, wr, r;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+
+	if (!buf)
+		return (0);
+
+	r = read(fd, buf, letters);
+
+	wr = write(STDOUT_FILENO, buf, r);
+
+	if (wr == -1)
+		return (0);
+
+	free(buf);
+
+	close(fd);
+
+	return (wr);
+}
