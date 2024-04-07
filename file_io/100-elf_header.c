61e66d59bb34aa32a3adcd64e0af19a03d05a874
Raid-dev <a_rauf2004@mail.ru>
2024-04-07 19:33:42 +0000

    Added executable script

--- file_io/100-elf_header.c
+++ file_io/100-elf_header.c
@@ -283,18 +283,14 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	Elf64_Ehdr *header;
 	int o, r;
-
 	o = open(argv[1], O_RDONLY);
-
 	if (o == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read file %s\n",
 				argv[1]);
 		exit(98);
 	}
-
 	header = malloc(sizeof(Elf64_Ehdr));
-
 	if (header == NULL)
 	{
 		close_elf(o);
@@ -302,9 +298,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 				argv[1]);
 		exit(98);
 	}
-
 	r = read(o, header, sizeof(Elf64_Ehdr));
-
 	if (r == -1)
 	{
 		free(header);
@@ -313,11 +307,8 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 				argv[1]);
 		exit(98);
 	}
-
 	check_elf(header->e_ident);
-
 	printf("ELF Header:\n");
-
 	print_magic(header->e_ident);
 	print_class(header->e_ident);
 	print_data(header->e_ident);
@@ -326,10 +317,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	print_abi(header->e_ident);
 	print_type(header->e_type, header->e_ident);
 	print_entry(header->e_entry, header->e_ident);
-
 	free(header);
-
 	close_elf(o);
-
 	return (0);
 }
