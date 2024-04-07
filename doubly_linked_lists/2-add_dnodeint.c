62921624f19049424b875741f124e2c9d37a529b
Raid-dev <a_rauf2004@mail.ru>
2024-04-07 09:48:00 +0000

    Added executable script

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * add_dnodeint - Adds a new node at the beginning of the dlistint_t list head
+ * @head: The dlistint_t list to add to
+ * @n: The integer of the new node
+ *
+ * Return: The new node.
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *node = (dlistint_t *)malloc(sizeof(dlistint_t));
+
+	if (node == NULL)
+		return (NULL);
+
+	node->n = n;
+	node->prev = NULL;
+	node->next = *head;
+
+	*head = node;
+
+	return (*head);
+}
