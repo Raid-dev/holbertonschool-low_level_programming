96675dc2ddec08e4e6eac66b284fa7c66cf9326b
Raid-dev <a_rauf2004@mail.ru>
2024-04-07 13:18:18 +0000

    Added executable script

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -88,9 +88,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	dlistint_t *node;
 
-	if (node == NULL)
-		return (NULL);
-
 	if (idx > dlistint_len(*h))
 		return (NULL);
 
